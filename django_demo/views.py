8aecdc9fbb5918ad2ca94e133902ebb599531737
jian.li <jian.li@dmall.com>
2020-08-21 07:17:46 -0400

    tt

--- django_demo/views.py
+++ django_demo/views.py
@@ -4,7 +4,6 @@ from django_demo import logger
 
 
 def index(request):
-    logger.info("1111111111111")
     if request.COOKIES.get('is_login'):
         return render(request, 'index.html')
     else:
